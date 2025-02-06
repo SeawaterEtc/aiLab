495c45c38bda408e59fe9fc0cd035d9650a18410
SeawaterEtc <cholsaseawater@gmail.com>
2025-02-06 10:44:14 +0700

    firt test

---
+++ hello.py
@@ -0,0 +1,16 @@
+
+# a function to say hello
+def say_hello():
+    print('Hello, World!')
+    
+# a function to say goodbye
+def say_goodbye():
+    print('Goodbye, World!')
+    
+# main function
+def main():
+    say_hello()
+    say_goodbye()
+
+# call the main function
+main()
