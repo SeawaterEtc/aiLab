96331183eebe040004321b313e07d3591e9a857f
SeawaterEtc <cholsaseawater@gmail.com>
2025-02-06 10:46:23 +0700

    modify

--- hello.py
+++ hello.py
@@ -3,14 +3,11 @@
 def say_hello():
     print('Hello, World!')
     
-# a function to say goodbye
-def say_goodbye():
-    print('Goodbye, World!')
     
 # main function
 def main():
     say_hello()
-    say_goodbye()
+    # say_goodbye()
 
 # call the main function
 main()
